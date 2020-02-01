dc45611aaa707802b04a994fbd19eee0e68657ba
Tymur <3brownjohn3@gmail.com>
2020-02-01 21:08:05 +0200

    Finally with working fwrite(problem was in if(f = NULL))

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <malloc.h>
 #include <string.h>
 #include <conio.h>
+#include "write.h"
 #define KEY_A 65
 #define KEY_O 79
 #define KEY_F 70
@@ -10,61 +11,42 @@
 #define KEY_S 83
 #define KEY_BACKSPACE 8 
 
-struct teacher{
-	char suname[15];
-	char name[15];
-	char otchestv[15];
-	char sex[15];
-	char city[15];
-	char addres[15];
-	int house;
-	int flat;
-	char birth[15];
-	char kafedra[15];
-	char posada[15];
-	char step[15];
-	char osvita[15];
-	int staz;
-};
-
+void write(str* num, int* am);
+void menu(void);
+void show_structure(str* num, int* am);
 int main()
 {
 	setvbuf(stdout, NULL, _IONBF, 0);
-	int am = 0, search, delete, task;
-	char name[] = "C:\\Users\\3brow\\Practise_Task\\teacher.txt";
-	struct teacher *num;
-	num = (struct teacher*)malloc(sizeof(struct teacher));
+	char name[] = "teacher.txt";
+	FILE* f;
+	f = fopen(name, "a+");
+	if (f == NULL)
 	{
-		
-		printf("Structure:\n");
-		printf("0 - Close\n");
-		printf("1 - New record\n");
-		printf("2 - Open structure\n");
-		printf("3 - Search\n");
-		printf("4 - Delete record\n");
-		printf("5 - Sort by suname\n-->");
+		printf("\nError with open file");
+		return 1;
+	}
+	int am = 0, task;
+	fseek(f, 0, SEEK_END);
+	int size = ftell(f);
+	rewind(f);
+	str num[50];
+
+	do {
+		menu();
 		scanf("%d", &task);
 		switch (task)
 		{
 			case 1:
-
-				// Write
-
-				FILE *f;
-				f = fopen(name, "a");
-				if (f == NULL) 
+				write(num, &am);
+				am++;
+				for (int i = 0; i < am; i++)
 				{
-					printf("\nFile cannot be reach\n");
-					break;									 	// файл не удалось открыть
+  					fwrite(&num[i], sizeof(str), am, f);
 				}
-				else 
-				{
-					num = write()
-					fwrite()				// Работа с файлом
-				}
+				
 				break;
 			case 2:
-				printf("\n2");
+				show_structure(num, &am);
 				break;
 			case 3:
 				printf("\n3");
@@ -76,13 +58,12 @@ int main()
 				printf("\n5");
 				break;
 			case 0:
-				free(num);
+				fclose(f);
 				return 0;
 			default:
 				printf("\nWrong number\n");
 				break;
 		}
 	}while (task != 0);
-	free(num);
 	return 0;
 }
--- write.h
+++ write.h
@@ -4,4 +4,23 @@
 #include <string.h>
 #include <conio.h>
 
-teacher write(teacher num);
+typedef struct teacher{
+	char suname[15];
+	char name[15];
+	char otchestv[15];
+	char sex[15];
+	char city[15];
+	char addres[15];
+	int house;
+	int flat;
+	char birth[15];
+	char kafedra[15];
+	char posada[15];
+	char step[15];
+	char osvita[15];
+	int staz;
+}str;
+
+void write(str* num, int* am);
+void menu(void);
+void show_structure(str* num, int* am);
